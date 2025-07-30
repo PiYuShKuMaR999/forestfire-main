efc1c91244e91ba33c1c996d9f7b4c5dbcecb445
PiYuShKuMaR999 <piyush990841@gmail.com>
2025-07-30 09:32:59 +0530

    forest fire model

--- application.py
+++ application.py
@@ -36,5 +36,4 @@ def predict_data():
         return render_template('home.html')
         
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0")
